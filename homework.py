399fb7a425f41193bd97e597003e374ebf675f44
Алена Виноградова <lepusred@mail.ru>
2022-04-22 23:37:49 +0300

    фитнес трекер 4

--- homework.py
+++ homework.py
@@ -14,11 +14,11 @@ class InfoMessage:
 
     def get_message(self) -> str:
         """Выводит сообщение"""
-        return str(f'Тип тренировки: {self.training_type};'
-                   + f' Длительность: {self.duration:.3f} ч.;'
-                   + f' Дистанция: {self.distance:.3f} км;'
-                   + f' Ср. скорость: {self.speed:.3f} км/ч;'
-                   + f' Потрачено ккал: {self.calories:.3f}.')
+        return (f'Тип тренировки: {self.training_type};'
+                f' Длительность: {self.duration:.3f} ч.;'
+                f' Дистанция: {self.distance:.3f} км;'
+                f' Ср. скорость: {self.speed:.3f} км/ч;'
+                f' Потрачено ккал: {self.calories:.3f}.')
 
 
 class Training:
@@ -45,7 +45,7 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        NotImplementedError
+        raise NotImplementedError
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -61,13 +61,6 @@ class Running(Training):
     CONST1 = 18
     CONST2 = 20
     DURATION_MIN = 60
-
-    def __init__(self, action: int,
-                 duration: float,
-                 weight: float) -> None:
-        super().__init__(action,
-                         duration,
-                         weight)
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -132,8 +125,11 @@ class Swimming(Training):
 def read_package(workout_type: str, data: List[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
     variant: Dict = {'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking}
-    my_train: variant[workout_type] = variant[workout_type](*data)
-    return my_train
+    if workout_type in variant:
+        my_train: variant[workout_type] = variant[workout_type](*data)
+        return my_train
+    else:
+        raise KeyError('Неверный код тренировки')
 
 
 def main(training: Training) -> None:
